c17642c6ceb807a6f4a9b96c81bd7a904cc6ca8d
RootstemC-PC <test>
2017-06-23 16:51:03 +0900

    으어ㅏ아아

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,10 +3,19 @@ package controller;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@Autowired
 	private SqlSession sqlSession;
+	
+	@RequestMapping("index.do")
+	public String index()throws Exception{
+		
+		System.out.println("메인랜딩페이지");
+		
+		return "main.index";
+	}
 }
